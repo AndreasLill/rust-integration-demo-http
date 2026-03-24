722d68d64445956a948e22900067120d11a2db61
Andreas Lill <dev@andreaslill.com>
2026-03-24 21:21:12 +0100

    Update dependency version and removed comments.

--- src/main.rs
+++ src/main.rs
@@ -17,38 +17,29 @@ async fn main() {
 // curl -i -H "Content-Type: application/json" -d '{"bucket":"my-bucket","key":"my-key"}' http://127.0.0.1:8080/xml
 async fn json_to_xml(request: HttpRequest) -> HttpResponse {
 
-    // Read the body as bytes into memory.
     let body = request.body_as_bytes().await.unwrap();
 
-    // Parse and deserialize body bytes as JSON into Document struct.
     let json: serde_json::Value = match serde_json::from_slice(&body) {
         Ok(json) => json,
         Err(_) => return HttpResponse::builder().status(400).body_bytes("Invalid JSON").unwrap(),
     };
 
-    // Parse and serialize Document struct into XML string.
     let xml = match quick_xml::se::to_string_with_root("Root", &json) {
         Ok(xml) => format!("<?xml version=\"1.0\" encoding=\"UTF-8\"?>{}", xml),
         Err(_) => return HttpResponse::builder().status(500).body_bytes("Could not parse XML").unwrap(),
     };
 
-    // Build http response with XML string as body.
     HttpResponse::builder().status(200).body_bytes(xml).unwrap()
 }
 
 // curl -i http://127.0.0.1:8080/proxy/helloworld
 async fn reverse_proxy(request: HttpRequest) -> HttpResponse {
 
-    // Get the {value} parameter from path.
     let value = request.params().get("value").unwrap();
-
-    // Format canonical uri with value.
+    
     let uri = format!("https://httpbin.org/anything/{}", value);
+    let req = HttpRequest::builder().get(uri).body_empty().unwrap();
 
-    // Create a new request with the canonical uri.
-    let req = HttpRequest::builder().uri(uri).method("GET").body_empty().unwrap();
-
-    // Send the request and handle response.
     match HttpClient::new().send(req).await {
         Ok(response) => response,
         Err(err) => HttpResponse::builder().status(500).body_bytes(err.to_string()).unwrap(),
@@ -58,19 +49,15 @@ async fn reverse_proxy(request: HttpRequest) -> HttpResponse {
 // curl -i -H "key: doc.txt" --data-binary @/home/andreas/file.txt http://127.0.0.1:8080/upload
 async fn upload(request: HttpRequest) -> HttpResponse {
 
-    // Get 'key' header.
     let key = match request.headers().get("key") {
         Some(key) => key.to_owned(),
         None => return HttpResponse::builder().status(400).body_bytes("Missing header: key").unwrap(),
     };
 
-    // Read the body as bytes into memory.
     let body = request.body_as_bytes().await.unwrap();
 
-    // Create S3 client config for use with minio.
     let config = S3ClientConfig::new("http://127.0.0.1:9000").access_key("minioadmin").secret_key("minioadmin");
 
-    // Put object as bytes using the S3 client.
     match S3Client::new(config).bucket("docs").put_object_bytes(key.to_str().unwrap(), body).await {
         Ok(_) => HttpResponse::builder().status(200).body_empty().unwrap(),
         Err(err) => HttpResponse::builder().status(500).body_bytes(err.to_string()).unwrap(),
@@ -80,16 +67,13 @@ async fn upload(request: HttpRequest) -> HttpResponse {
 // curl -i -H "key: doc.txt" http://127.0.0.1:8080/download
 async fn download(request: HttpRequest) -> HttpResponse {
 
-    // Get 'key' header.
     let key = match request.headers().get("key") {
         Some(key) => key.to_owned(),
         None => return HttpResponse::builder().status(400).body_bytes("Missing header: key").unwrap(),
     };
 
-    // Create S3 client config for use with minio.
     let config = S3ClientConfig::new("http://127.0.0.1:9000").access_key("minioadmin").secret_key("minioadmin");
     
-    // Put object as bytes using the S3 client.
     match S3Client::new(config).bucket("docs").get_object_bytes(key.to_str().unwrap()).await {
         Ok(bytes) => HttpResponse::builder().status(200).body_bytes(bytes).unwrap(),
         Err(err) => HttpResponse::builder().status(500).body_bytes(err.to_string()).unwrap(),
